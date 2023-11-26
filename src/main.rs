9145b55c7a5bfb1c1bc746194170f5263e946d7e
Jason Tomsic <jasontomsic@quickenloans.com>
2023-11-26 07:48:11 -0500

    press any key to generate a new loadout

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use crossterm::{
-    execute,
-    style::Stylize,
-    terminal::{Clear, ClearType},
-};
+use crossterm::event::{Event, KeyCode, KeyModifiers};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::{execute, style::Stylize};
 use rand::prelude::SliceRandom;
 use serde::Deserialize;
-use serde_yaml::from_reader;
 use std::{fs::File, io::stdout};
 
 #[derive(Deserialize)]
@@ -17,29 +14,52 @@ struct Survivor {
     special: Vec<String>,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut rng = rand::thread_rng();
 
     let file = File::open("survivors.yml").unwrap();
-    let list: Vec<Survivor> = from_reader(file).unwrap();
+    let list: Vec<Survivor> = serde_yaml::from_reader(file).unwrap();
 
-    let survivor = list.choose(&mut rng).unwrap();
-    let primary = survivor.primary.choose(&mut rng).unwrap();
-    let secondary = survivor.secondary.choose(&mut rng).unwrap();
-    let utility = survivor.utility.choose(&mut rng).unwrap();
-    let special = survivor.special.choose(&mut rng).unwrap();
+    loop {
+        let survivor = list.choose(&mut rng).unwrap();
+        let primary = survivor.primary.choose(&mut rng).unwrap();
+        let secondary = survivor.secondary.choose(&mut rng).unwrap();
+        let utility = survivor.utility.choose(&mut rng).unwrap();
+        let special = survivor.special.choose(&mut rng).unwrap();
 
-    execute!(stdout(), Clear(ClearType::All)).ok();
+        execute!(stdout(), Clear(ClearType::All))?;
 
-    println!(
-        "{} {}",
-        "Survivor:".blue(),
-        survivor.name.to_string().bold().red()
-    );
-    println!("{}", "-------".dark_grey());
-    println!("  {}    {}", "primary:".blue(), primary.to_string().red());
-    println!("  {}  {}", "secondary:".blue(), secondary.to_string().red());
-    println!("  {}    {}", "utility:".blue(), utility.to_string().red());
-    println!("  {}    {}", "special:".blue(), special.to_string().red());
-    println!("{}", "-------".dark_grey());
+        println!(
+            "{}     {}",
+            "Survivor:".blue(),
+            survivor.name.to_string().bold().red(),
+        );
+        println!("{}", "-------".dark_grey());
+        println!("  {}    {}", "primary:".blue(), primary.to_string().red());
+        println!("  {}  {}", "secondary:".blue(), secondary.to_string().red());
+        println!("  {}    {}", "utility:".blue(), utility.to_string().red());
+        println!("  {}    {}", "special:".blue(), special.to_string().red());
+        println!("{}", "-------".dark_grey());
+        println!();
+        println!("{}", "press any key for a new loadout");
+        println!("{}", "press esc to exit");
+
+        enable_raw_mode()?;
+        match crossterm::event::read().unwrap() {
+            Event::Key(event) => {
+                if [KeyCode::Esc, KeyCode::Char('q')].contains(&event.code) {
+                    break;
+                }
+
+                if event.code == KeyCode::Char('c') && event.modifiers == KeyModifiers::CONTROL {
+                    break;
+                }
+            }
+            _ => {}
+        }
+        disable_raw_mode()?;
+    }
+    disable_raw_mode()?;
+
+    return Ok(());
 }
